d226cf14ca9ad61546a4200400e0d99a89d30d7b
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-06 12:37:08 +0100

    Created tests.

---
+++ src/RequestLogger.Serilog.Tests/Formatters/DefaultMessageFormatterTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RequestLogger.Serilog.Formatters;
+
+namespace RequestLogger.Serilog.Tests.Formatters
+{
+    [TestFixture]
+    [Parallelizable]
+    public class DefaultMessageFormatterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _formatter = new DefaultMessageFormatter();
+        }
+
+        private IMessageFormatter _formatter;
+
+        [Test]
+        public void Format_ShouldReturn_FormattedMessage_With_MessageTemplate()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+
+            var result = _formatter.Format(requestData, responseData);
+
+            Assert.AreEqual(
+                "{HttpMethod} {Url} {RequestHeader} {RequestContent} {StatusCode} {ReasonPhrase} {ResponseHeader} {ResponseContent}",
+                result.MessageTemplate);
+        }
+
+        [Test]
+        public void Format_ShouldReturn_FormattedMessage_With_Properties()
+        {
+            const string requestMessage = "Ping";
+            const string responseMessage = "Pong";
+            var requestData = new RequestData
+            {
+                HttpMethod = "GET",
+                Url = new Uri("http://ekm.com"),
+                Header = new Dictionary<string, string[]>
+                {
+                    {"Content-Type", new[] {"application/json"}}
+                },
+                Content = Encoding.UTF8.GetBytes(requestMessage)
+            };
+            var responseData = new ResponseData
+            {
+                StatusCode = 400,
+                ReasonPhrase = "Bad Request",
+                Header = new Dictionary<string, string[]>(),
+                Content = Encoding.UTF8.GetBytes(responseMessage)
+            };
+
+            var result = _formatter.Format(requestData, responseData);
+
+            Assert.AreEqual(8, result.Properties.Length);
+            Assert.AreEqual(requestData.HttpMethod, result.Properties[0]);
+            Assert.AreEqual(requestData.Url, result.Properties[1]);
+            Assert.AreEqual(requestData.Header, result.Properties[2]);
+            Assert.AreEqual(requestMessage, result.Properties[3]);
+            Assert.AreEqual(responseData.StatusCode, result.Properties[4]);
+            Assert.AreEqual(responseData.ReasonPhrase, result.Properties[5]);
+            Assert.AreEqual(responseData.Header, result.Properties[6]);
+            Assert.AreEqual(responseMessage, result.Properties[7]);
+        }
+
+        [Test]
+        public void Format_WithException_ShouldReturn_FormattedMessage_With_MessageTemplate()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+            var ex = new Exception();
+
+            var result = _formatter.Format(requestData, responseData, ex);
+
+            Assert.AreEqual(
+                "{HttpMethod} {Url} {RequestHeader} {RequestContent} {StatusCode} {ReasonPhrase} {ResponseHeader} {ResponseContent} {errorSource} {errorMessage} {errorStackTrace}",
+                result.MessageTemplate);
+        }
+
+        [Test]
+        public void Format_WithException_ShouldReturn_FormattedMessage_With_Properties()
+        {
+            const string requestMessage = "Ping";
+            const string responseMessage = "Pong";
+            var requestData = new RequestData
+            {
+                HttpMethod = "GET",
+                Url = new Uri("http://ekm.com"),
+                Header = new Dictionary<string, string[]>
+                {
+                    {"Content-Type", new[] {"application/json"}}
+                },
+                Content = Encoding.UTF8.GetBytes(requestMessage)
+            };
+            var responseData = new ResponseData
+            {
+                StatusCode = 400,
+                ReasonPhrase = "Bad Request",
+                Header = new Dictionary<string, string[]>(),
+                Content = Encoding.UTF8.GetBytes(responseMessage)
+            };
+            var ex = new Exception("Test Error") { Source = "Test" };
+
+            var result = _formatter.Format(requestData, responseData, ex);
+
+            Assert.AreEqual(11, result.Properties.Length);
+            Assert.AreEqual(requestData.HttpMethod, result.Properties[0]);
+            Assert.AreEqual(requestData.Url, result.Properties[1]);
+            Assert.AreEqual(requestData.Header, result.Properties[2]);
+            Assert.AreEqual(requestMessage, result.Properties[3]);
+            Assert.AreEqual(responseData.StatusCode, result.Properties[4]);
+            Assert.AreEqual(responseData.ReasonPhrase, result.Properties[5]);
+            Assert.AreEqual(responseData.Header, result.Properties[6]);
+            Assert.AreEqual(responseMessage, result.Properties[7]);
+            Assert.AreEqual(ex.Source, result.Properties[8]);
+            Assert.AreEqual(ex.Message, result.Properties[9]);
+            Assert.AreEqual(ex.StackTrace, result.Properties[10]);
+        }
+    }
+}
---
+++ src/RequestLogger.Serilog.Tests/SerilogLoggerTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using RequestLogger.Serilog.Formatters;
+using Serilog;
+
+namespace RequestLogger.Serilog.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class SerilogLoggerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new Mock<ILogger>();
+            _messageFormatter = new Mock<IMessageFormatter>();
+            _requestLogger = new SerilogLogger(_logger.Object, _messageFormatter.Object);
+
+        }
+
+        private Mock<ILogger> _logger;
+        private Mock<IMessageFormatter> _messageFormatter;
+        private IRequestLogger _requestLogger;
+        
+        [Test]
+        public void Log_ShouldCall_Logger_Information()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+            var formattedMessage = new FormattedMessage {MessageTemplate = "1"};
+
+            _messageFormatter
+                .Setup(x => x.Format(requestData, responseData))
+                .Returns(formattedMessage);
+
+            _requestLogger.Log(requestData, responseData);
+
+            _logger.Verify(x => x.Information(formattedMessage.MessageTemplate, formattedMessage.Properties), Times.Once);
+        }
+
+        [Test]
+        public void Log_When_FormattedMessageHasNoContent_ShouldNotCall_Logger_Information()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+
+            _messageFormatter
+                .Setup(x => x.Format(requestData, responseData))
+                .Returns(new FormattedMessage());
+
+            _requestLogger.Log(requestData, responseData);
+
+            _logger.Verify(x => x.Information(It.IsAny<string>(), It.IsAny<object[]>()), Times.Never);
+        }
+
+        [Test]
+        public void LogError_ShouldCall_Logger_Error()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+            var ex = new Exception();
+            var formattedMessage = new FormattedMessage {MessageTemplate = "1"};
+
+            _messageFormatter
+                .Setup(x => x.Format(requestData, responseData, ex))
+                .Returns(formattedMessage);
+
+            _requestLogger.LogError(requestData, responseData, ex);
+
+            _logger.Verify(x => x.Error(formattedMessage.MessageTemplate, formattedMessage.Properties), Times.Once);
+        }
+
+        [Test]
+        public void LogError_When_FormattedMessageHasNoContent_ShouldNotCall_Logger_Error()
+        {
+            var requestData = new RequestData();
+            var responseData = new ResponseData();
+            var ex = new Exception();
+
+            _messageFormatter
+                .Setup(x => x.Format(requestData, responseData))
+                .Returns(new FormattedMessage());
+
+            _requestLogger.LogError(requestData, responseData, ex);
+
+            _logger.Verify(x => x.Error(It.IsAny<string>(), It.IsAny<object[]>()), Times.Never);
+        }
+    }
+}
