96563c8be0f22efd18a9bddb5bda11473c081c5c
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-20 14:31:25 +0100

    Added an ASP.NET MVC example.

---
+++ src/Examples/AspNetExample/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using RequestLogger.Serilog;
+using RequestLogger.Web;
+using Serilog;
+
+namespace AspNetExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly RequestLoggerModule RequestLoggerModule = CreateRequestLoggerModule();
+
+        public override void Init()
+        {
+            base.Init();
+
+            RequestLoggerModule.Init(this);
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private static RequestLoggerModule CreateRequestLoggerModule()
+        {
+            var logger = new LoggerConfiguration().WriteTo.Trace().CreateLogger();
+            var requestLogger = new SerilogLogger(logger);
+            return new RequestLoggerModule(requestLogger);
+        }
+    }
+}
